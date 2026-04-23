6d96654f5cc3db9ad645bb8a99a8271dfbf6f51d
sanju marik <jackiehsj32@gmail.com>
2026-04-23 22:01:03 +0530

    cron job added

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+// Health check route
+app.get('/health', (req, res) => {
+  try{
+    console.log("Health OK");
+  res.status(200).json({ message: 'Server is running' });
+  }
+  catch(err){
+    console.error('Error in health check:', err);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/profiles', profileRoutes);
 
@@ -27,6 +39,10 @@ mongoose.connect(MONGO_URI)
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
+
+    setInterval(async() => {
+        await fetch(`https://dream-matrimony-server.onrender.com/health`,{method:"GET"});
+    }, 14 * 60 *1000);
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
